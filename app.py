6f69b85dd775e6185b4a11e8608827e54c60f8b2
Kaustav Ghosh Dastidar <kaustav.gd@outlook.com>
2023-06-22 01:01:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -32,89 +32,89 @@ A profitable trade may or may not have been a good trade
 """
 )
 
-st.header("**Selected Price**")
+# st.header("**Selected Price**")
 
-# Load market data from Binance API
-df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
+# # Load market data from Binance API
+# df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
 
-# Custom function for rounding values
-def round_value(input_value):
-    if input_value.values > 1:
-        a = float(round(input_value, 2))
-    else:
-        a = float(round(input_value, 8))
-    return a
+# # Custom function for rounding values
+# def round_value(input_value):
+#     if input_value.values > 1:
+#         a = float(round(input_value, 2))
+#     else:
+#         a = float(round(input_value, 8))
+#     return a
 
 
-crpytoList = {
-    "Price 1": "BTCBUSD",
-    "Price 2": "ETHBUSD",
-    "Price 3": "BNBBUSD",
-    "Price 4": "XRPBUSD",
-    "Price 5": "ADABUSD",
-    "Price 6": "DOGEBUSD",
-    "Price 7": "SHIBBUSD",
-    "Price 8": "DOTBUSD",
-    "Price 9": "MATICBUSD",
-}
+# crpytoList = {
+#     "Price 1": "BTCBUSD",
+#     "Price 2": "ETHBUSD",
+#     "Price 3": "BNBBUSD",
+#     "Price 4": "XRPBUSD",
+#     "Price 5": "ADABUSD",
+#     "Price 6": "DOGEBUSD",
+#     "Price 7": "SHIBBUSD",
+#     "Price 8": "DOTBUSD",
+#     "Price 9": "MATICBUSD",
+# }
 
-col1, col2, col3 = st.columns(3)
+# col1, col2, col3 = st.columns(3)
 
-for i in range(len(crpytoList.keys())):
-    selected_crypto_label = list(crpytoList.keys())[i]
-    selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
-    selected_crypto = st.sidebar.selectbox(
-        selected_crypto_label, df.symbol, selected_crypto_index, key=str(i)
-    )
-    col_df = df[df.symbol == selected_crypto]
-    col_price = round_value(col_df.weightedAvgPrice)
-    col_percent = f"{float(col_df.priceChangePercent)}%"
-    if i < 3:
-        with col1:
-            st.metric(selected_crypto, col_price, col_percent)
-    if 2 < i < 6:
-        with col2:
-            st.metric(selected_crypto, col_price, col_percent)
-    if i > 5:
-        with col3:
-            st.metric(selected_crypto, col_price, col_percent)
+# for i in range(len(crpytoList.keys())):
+#     selected_crypto_label = list(crpytoList.keys())[i]
+#     selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
+#     selected_crypto = st.sidebar.selectbox(
+#         selected_crypto_label, df.symbol, selected_crypto_index, key=str(i)
+#     )
+#     col_df = df[df.symbol == selected_crypto]
+#     col_price = round_value(col_df.weightedAvgPrice)
+#     col_percent = f"{float(col_df.priceChangePercent)}%"
+#     if i < 3:
+#         with col1:
+#             st.metric(selected_crypto, col_price, col_percent)
+#     if 2 < i < 6:
+#         with col2:
+#             st.metric(selected_crypto, col_price, col_percent)
+#     if i > 5:
+#         with col3:
+#             st.metric(selected_crypto, col_price, col_percent)
 
-st.header("")
+# st.header("")
+
+# # st.download_button(
+# #    label="Download data as CSV",
+# #    data=df,
+# #    #file_name='large_df.csv',
+# #    # mime='text/csv'
+# #    )
+
+
+# @st.cache_data()
+# def convert_df(df):
+#     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+#     return df.to_csv().encode("utf-8")
+
+
+# csv = convert_df(df)
 
 # st.download_button(
-#    label="Download data as CSV",
-#    data=df,
-#    #file_name='large_df.csv',
-#    # mime='text/csv'
-#    )
+#     label="Download data as CSV",
+#     data=csv,
+#     file_name="large_df.csv",
+#     mime="text/csv",
+# )
+
+# st.dataframe(df, height=2000)
 
 
-@st.cache_data()
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode("utf-8")
-
-
-csv = convert_df(df)
-
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name="large_df.csv",
-    mime="text/csv",
-)
-
-st.dataframe(df, height=2000)
-
-
-st.markdown(
-    """
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-""",
-    unsafe_allow_html=True,
-)
+# st.markdown(
+#     """
+# <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+# <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+# <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+# """,
+#     unsafe_allow_html=True,
+# )
 
 
 
